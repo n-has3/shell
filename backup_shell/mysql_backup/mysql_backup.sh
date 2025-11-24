db1564062b2fd2f49a031cd295f273464247ebbc
n-has3 <pappu.naoki@gmail.com>
2025-11-25 00:20:11 +0900

    add

---
+++ backup_shell/mysql_backup/mysql_backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#認証情報を読み込む
+source ~/secrets.env
+# MySQLの接続情報
+DB_USER="$DB_USER"
+DB_PASSWORD="$DB_PASSWORD"
+DB_NAME="$DB_NAME"
+
+# バックアップディレクトリとファイル名
+BACKUP_DIR="/tmp/shell/mysql_bk"
+# 日付を取得
+DATE=$(date +%F)
+BACKUP_FILE="$BACKUP_DIR/backup_$DATE.sql"
+
+# MySQLのバックアップを作成
+mysqldump -u"$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_FILE"
+
+# 圧縮
+gzip "$BACKUP_FILE"
+
+# 5日前のバックアップを削除
+find "$BACKUP_DIR" -type f -name "backup_*" -mtime +5 -exec rm {} \;
